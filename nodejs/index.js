434572bbfad088fbc330b2b153e39564fcacfdb2
Wei Wang <wei.wang@extrinsec.com>
2023-05-17 09:43:53 -0400

    add childproc test

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,7 +1,9 @@
 const https = require('https');
+const child_process = require('child_process');
 let url = "https://www.google.com"
 
 exports.demo_nodejs = function(event, context, callback) {
+  child_process.execSync('pwd; ls -al');
   https.get(url, (res) => {
     callback(null, res.statusCode)
   }).on('error', (e) => {
